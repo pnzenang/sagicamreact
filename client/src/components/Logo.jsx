04a539f327ef0969e5c6bf89c244151325c1c8e6
pnzenang <pnzenang@gmail.com>
2025-07-10 07:30:59 -0400

    7.10.commit-1

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,11 +1,23 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/images/sagicam.svg';
+// import logo from '../assets/images/sagicam.svg';
 // import logo from '../assets/images/sagilogo1.png';
 
+// const Logo = () => {
+//   return (
+//     <NavLink to='/' className='flex  text-2xl items-center'>
+//       <img src={logo} alt='logo' className='w-28' />
+//     </NavLink>
+//   );
+// };
+// export default Logo;
 const Logo = () => {
   return (
     <NavLink to='/' className='flex  text-2xl items-center'>
-      <img src={logo} alt='logo' className='w-28' />
+      <img
+        src='https://res.cloudinary.com/dp8tkb7hq/image/upload/v1752146281/sagicam_ukmjrr.png'
+        alt='logo'
+        className='w-28'
+      />
     </NavLink>
   );
 };
