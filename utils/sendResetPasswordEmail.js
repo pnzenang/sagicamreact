9fdc1ad9560363b1154dac71774ad088b363ee21
pnzenang <pnzenang@gmail.com>
2025-09-05 21:53:01 -0400

    9.6.commit

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -11,7 +11,7 @@ export const sendResetPasswordEmail = async ({
 
   return sendEmail({
     to: email,
-    from: 'info@mysagi.org',
+    from: 'info@sagicam.org',
     subject: 'Reset Password',
     html: `<h3>Hello, ${name}</h3>${message}`,
   });
