87626bdb065910fbe15df24927bd08e8d358a074
pnzenang <pnzenang@gmail.com>
2025-10-05 01:32:39 -0400

    10.4.commit

--- client/src/pages/Contributions.jsx
+++ client/src/pages/Contributions.jsx
@@ -2,10 +2,10 @@ const Contributions = () => {
   return (
     <div>
       <iframe
-        height={800}
         width='100%'
+        height={800}
         className='mx-auto'
-        src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQTa_678Qg3VLM61-M8a15J_OP70fu_fHsJdRicHfXfrNPkQtGc9j-UD8cDWZKNML7rNMpMyDM08pQy/pubhtml?gid=84065004&single=true'
+        src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQTa_678Qg3VLM61-M8a15J_OP70fu_fHsJdRicHfXfrNPkQtGc9j-UD8cDWZKNML7rNMpMyDM08pQy/pubhtml?gid=84065004&amp;single=true&amp;widget=true&amp;headers=false'
       ></iframe>
     </div>
   );
--- client/src/pages/VestedMembers.jsx
+++ client/src/pages/VestedMembers.jsx
@@ -2,7 +2,7 @@ const VestedMembers = () => {
   return (
     <div className='h-full '>
       <iframe
-        width='65%'
+        width='100%'
         height='800'
         className='mx-auto'
         src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQTa_678Qg3VLM61-M8a15J_OP70fu_fHsJdRicHfXfrNPkQtGc9j-UD8cDWZKNML7rNMpMyDM08pQy/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false'
