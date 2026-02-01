6485bdbec59acc8636e387aa82c3e732eec9b7b7
pnzenang <49215536+pnzenang@users.noreply.github.com>
2026-02-01 12:13:25 -0500

    Clarify membership benefits based on duration
    Updated membership duration and benefit amounts for clarity.

--- client/src/pages/How.jsx
+++ client/src/pages/How.jsx
@@ -135,9 +135,9 @@ export default function TabsRender() {
               </summary>
               <p className='px-4 py-6 pt-0 ml-4 -mt-4 text-primary'>
                 It really depends on how long the member has been a member; for
-                membership with matriculation number, between 0 to 6 months, the
-                family receives up to $500, between 7 t0 12 months the family
-                receive $1,000 and beyond 12 months the family receives $6,000
+                membership with matriculation number, between 30 days to 180 days, the
+                family receives up to $1000, between 181 t0 365 days the family
+                receive $2,000 and beyond 12 months the family receives $6,000
               </p>
             </details>
             <details className='w-full border rounded-lg'>
