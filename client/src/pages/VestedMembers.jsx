3d84f739e833d1c1a5f887854b38b8055db1978a
pnzenang <49215536+pnzenang@users.noreply.github.com>
2026-02-01 19:20:04 -0500

    Remove unused user context in AddMembers component

--- client/src/pages/VestedMembers.jsx
+++ client/src/pages/VestedMembers.jsx
@@ -1,5 +1,5 @@
 const AddMembers = () => {
-  const { user } = useOutletContext();
+
   return (
     <section className='h-full w-full  place-items-center mt-20 '>
       ‎Follow the SAGICAM-UPDATES channel on WhatsApp: https://whatsapp.com/channel/0029Vb79q1vDp2QA0Iq8M608
